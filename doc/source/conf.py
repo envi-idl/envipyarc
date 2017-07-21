d69a4e01901ae79ca4f0006d668fe81bef0dd65d
Eric Lefebvre <elefebvr@harris.com>
2017-07-21 12:19:55 -0600

    Updating documentation to reference envipyarclib

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -159,4 +159,5 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'python': ('https://docs.python.org/', None),
+                       'envipyarclib': ('https://envi-py-for-arcgis-library.readthedocs.io/en/latest/', None)}
--- envipyarc/gptoolbox.py
+++ envipyarc/gptoolbox.py
@@ -1,5 +1,9 @@
 """
-ENVI GPToolbox help goes heres
+The ENVI GPToolbox class is used to create python toolboxes capable of running \
+ENVI/IDL analytics inside of ArcMap and ArcGIS Pro.  Each generated toolbox contains \
+one or more GPTools that are lightweight wrappers to running ENVI/IDL tasks through \
+the taskengine.  For more api documentation on GPToolbox you can \
+view the envipyarclib :mod:`GPToolbox <envipyarclib:envipyarclib.gptoolbox>` class.
 """
 from string import Template
 from envipyarclib import GPToolbox as BaseToolbox
@@ -33,7 +37,8 @@ _EXECUTE_TEMPLATE = Template('''
 
 class GPToolbox(BaseToolbox):  # pylint: disable=too-few-public-methods
     """
-    Implementation of the GPToolbox class for ENVI/IDL.
+    Implementation of the envipyarclib \
+    :mod:`GPToolbox <envipyarclib:envipyarclib.gptoolbox>` class for ENVI/IDL.
     """
     def __init__(self, tasks=None, alias='envi'):
         super(GPToolbox, self).__init__(tasks,
